f4d4bbd33e859102399bae851e90340666a9a86b
StevePlayzz <65874635+StevePlayzz@users.noreply.github.com>
2020-10-26 22:12:53 -0400

    Adding Folders

---
+++ src/main/java/at/nuxnux/skinnedlanterns/config/Config.java
@@ -0,0 +1,26 @@
+package at.nuxnux.skinnedlanterns.config;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+import java.io.File;
+
+@Mod.EventBusSubscriber
+public class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec CONFIG;
+
+    static {
+        LanternConfig.init(BUILDER);
+
+        CONFIG = BUILDER.build();
+    }
+
+    public static void loadConfig(ForgeConfigSpec config, String path) {
+        final CommentedFileConfig file = CommentedFileConfig.builder(new File(path)).sync().autosave().writingMode(WritingMode.REPLACE).build();
+        file.load();
+        config.setConfig(file);
+    }
+}
